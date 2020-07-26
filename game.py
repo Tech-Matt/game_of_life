b5d46c14c4bbcfb3179d35b374972ca95da22142
Tech-Matt <mattia.rizzo.un@gmail.com>
2020-07-26 14:47:00 +0200

    Completed translation

--- game.py
+++ game.py
@@ -1,32 +1,31 @@
-#Game of Life
+#Conway Game of Life
 
 import pygame
 import random
 
 
-#INIZIALIZZAZIONE PYGAME
+#PYGAME INITIALIZATION
 success, failure = pygame.init()
 
 screen_width = 800
 screen_height = 600
 
-screen = pygame.display.set_mode((screen_width, screen_height)) #Inizializza lo schermo
-time = pygame.time.Clock() #Tiene traccia del tempo trascorso
+screen = pygame.display.set_mode((screen_width, screen_height)) #Init the screen
+time = pygame.time.Clock() #Time from startup
 FPS = 5
 
-#Area = 480000 px
-#Cell of area = 100px --> 4800 Cell
+#Screen Area = 480000 px (width * height)
+#Area of a cell = 100px --> 4800 Cell
 
-BLACK = (0, 0, 0)#Cella Viva
-WHITE = (255, 255, 255)#Cella morta
+BLACK = (0, 0, 0)#Live cell
+WHITE = (255, 255, 255)#dead cell
 
 
-#Classe di celle
 class Cell:
-    """ x: ascissa
-        y: ordinata
-        size: grandezza
-        alive: int (boolean, 0 o 1) flag per indicare lo stato della cella (morta, viva), all'inizio e' casuale
+    """ x: x coordinate
+        y: y coordinate
+        size: width and height (same)
+        alive: int (boolean, 0 o 1), to track the status of a cell (live or dead), at the startup is random
     """
     def __init__(self, x, y, alive):
         self.x = x
@@ -42,11 +41,11 @@ class Cell:
 def checkAlive(cell, cellArray, curr_x, curr_y, counter):
     """ Check wheter the current cell near the original cell is alive. If it is alive it adds 1 to the counter
 
-        cell: object of the original cell
+        cell: instance of the original cell
         cellArray: cell list with all the initialized cells
         curr_x: x coordinate of the cell which will be examined
         curr_y: y coordinate of the cell which will be examined
-        counter: variable that is updated whenever a cell near to original has the "alive" attribute = 1
+        counter: variable that is updated whenever a cell near to original has the "alive" attribute == 1
     """
 
     for current_cell in cellArray:
@@ -55,199 +54,200 @@ def checkAlive(cell, cellArray, curr_x, curr_y, counter):
                 counter += 1
 
 
-#Funzione per trovare i vicini delle celle ---------------------------------------------------
+#Function to find the neighbours of a cell ---------------------------------------------------
 
 def neighbour(cells, cell):
-    """ Da in output il numero di vicini di cell che hanno l'attributo alive = 1
+    """Give as output the number of neighbours of a cell (only neighbours with the alive attribute = 1)
 
-        cells: lista che contiene tutte le istanze di tipi cella inizializzate
-        cell: l'istanza di cui dobbiamo determinare i vicini 'vivi'
+        cells: List containing all the instances of the initialized cells
+        cell: The single instance of cell which will be examined to determine the number of live neighbours
 
         return: number of live neighbours
     """
-    num_neighbours = 0 #Numero di celle vicine vive (vicinanza di Moore)
+    num_neighbours = 0 #Number of near live cells(Moore neighbourhood)
     x = cell.x
     y = cell.y
 
-    #Angolo in alto a sinistra (x = 0, y = 0) !*!*!*!*!*!*!*!*!**!*!*!*!*!*!*!*
+    #List of exceptions before the main algorithm
+    #Upper-left corner (x = 0, y = 0) !*!*!*!*!*!*!*!*!**!*!*!*!*!*!*!*
     if (x == 0 and y == 0):
-        #Cella alla sua destra -----------
+        #Cell on the right -----------
         current_x = 1
         current_y = 0
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current ----------------------------------------
+        #Cell below current ----------------------------------------
         current_x = 1
         current_y = 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a quella iniziale
+        #Cell below original cell
         current_x = 0
         current_y = 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Ritorno il numero di vicini
+        #Return the number of neighbours
         return num_neighbours
 
-    #Angolo in alto a destra (x = finestra, y = 0)!*!*!*!*!**!*!*!*!*!*!*!*!*!*!*!*!*!*!**!*!*!*!*!*!*!*!
+    #Upper-right corner (x = window, y = 0)!*!*!*!*!**!*!*!*!*!*!*!*!*!*!*!*!*!*!**!*!*!*!*!*!*!*!
     elif (x == screen_width - cell.size and y == 0):
-        #Cella sotto -------------------------------------
+        #Cell below -------------------------------------
         current_x = screen_width - cell.size
         current_y = 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di current -----------------------------------
+        #Cell to the left of current -----------------------------------
         current_x -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di original
+        #Cell to the left of original
         current_y = 0
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Ritorno il numero di vicini
+        #Return the number of neighbours
         return num_neighbours
 
-    #Angolo in basso a sinistra (x = 0, y = finestra) !*!*!*!**!*!!*!**!*!!**!*!*!*!*!*
+    #Lower-left corner (x = 0, y = window) !*!*!*!**!*!!*!**!*!!**!*!*!*!*!*
     elif(x == 0 and y == (screen_height - cell.size)):
 
-        #Cella sopra a original ----------------------
+        #Cell over original ----------------------
         current_x = 0
         current_y = (screen_height - cell.size) - 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current ------------------------------------------
+        #Cell to the right of current ------------------------------------------
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current ---------------------------------------------
+        #Cell below current ---------------------------------------------
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Ritorno il numero di vicini
+        #Return the number of neighbours
         return num_neighbours
 
 
 
-    #Angolo in basso a destra !*!*!*!*!*!!*!*!*!*!*!*!**!!*!*!*
+    #Lower right corner !*!*!*!*!*!!*!*!*!*!*!*!**!!*!*!*
     elif (x == (screen_width - cell.size) and y == (screen_height - cell.size)):
 
-        #Cella a sinistra di original ------------------------------------------------
+        #Cell to the left of original ------------------------------------------------
         current_x = (screen_width - cell.size) - 1
         current_y = screen_height - cell.size
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sopra a current -------------------------------------------------------
+        #Cell on top of current -------------------------------------------------------
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current
+        #Cell to the right of current
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Ritorno il numero di vicini
+        #Return the number of neighbours
         return num_neighbours
 
 
-    #Se la cella si trova nella prima riga (y = 0) (e non e' nei 2 angoli) !*!*!*!*!*!!*!!*!*!*!*!
+    #If the cell is in the first row (y = 0) (2 corners excluded) !*!*!*!*!*!!*!!*!*!*!*!
     elif (y == 0 and (x != 0 and x != (screen_width - cell.size))):
-        #Cella a destra di original
+        #Cell to the right of original
         current_x = x + 1
         current_y = 0
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current
+        #Cell below current
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a original
+        #Cell below original
         current_x = x
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di current
+        #Cell to the left of current
         current_x -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di original
+        #Cell to the left of original
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Ritorno il numero di vicini
+        #Return the number of neighbours
         return num_neighbours
 
 
-    #Se la cella si trova nell'ultima riga (y = screen_height) e non nei due angoli !*!*!*!*!*!*!*!!*!*
+    #If the cell is in the last row (y = screen_height) 2 corners excluded !*!*!*!*!*!*!*!!*!*
     elif (y == (screen_height - cell.size) and (x != 0 and x != (screen_width - cell.size))):
-        #Cella a sinistra di original
+        #Cell to the left of original
         current_x = x - 1
         current_y = y
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sopra a current
+        #Cell on top of current
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current
+        #Cell to the right of current
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current
+        #Cell to the right of current
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current
+        #Cell below current
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Ritorno il numero di vicini
+        #Return the number of neighbours
         return num_neighbours
 
 
-    #Se la cella si trova nella prima colonna (e non nei due angoli) !*!*!*!*!*!*!*!*!*!*!*!*
+    #If the cell is in the first column (2 corners excluded) !*!*!*!*!*!*!*!*!*!*!*!*
     elif (x == 0 and (y != 0 and y != (screen_height - cell.size))):
-        #Cella sopra a original
+        #Cell on top of original
         current_x = x
         current_y = y - 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current
+        #Cell to the right of current
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current
+        #Cell below current
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current
+        #Cell below current
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di current
+        #Cell to the left of current
         current_x -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
@@ -256,30 +256,30 @@ def neighbour(cells, cell):
         return num_neighbours
 
 
-    #Se la cella si trova nell'ultima colonna (x = screen width) !*!*!*!*!*!*!*!!**!!*
+    #If the cell is in the last column (x = screen width) !*!*!*!*!*!*!*!!**!!*
     elif (x == (screen_width - cell.size) and (y != 0 and y != (screen_height - cell.size))):
-        #Cella sotto a original
+        #Cell below original
         current_x = x
         current_y = y + 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di current
+        #Cell to the left of current
         current_x -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sopra a current
+        #Cell on top of current
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sopra a current
+        #Cell on top of current
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current
+        #Cell to the right of current
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
@@ -287,46 +287,46 @@ def neighbour(cells, cell):
         return num_neighbours
 
 
-    #CASO GENERALE
+    #GENERAL RULE
     else:
-        #8 Vicini
-        #Cella sopra a original
+        #8 Neighbours
+        #Cell on top of original
         current_x = x
         current_y = y - 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a destra di current
+        #Cell to the right of original
         current_x += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current
+        #Cell below current
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sotto a current
+        #Cell below current
         current_y += 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di current
+        #Cell to the left of current
         current_x -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella a sinistra di current
+        #Cell to the left of current
         current_x -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sopra a current
+        #Cell on top of current
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
 
-        #Cella sopra a current
+        #Cell on top of current
         current_y -= 1
 
         checkAlive(cell, cells, current_x, current_y, num_neighbours)
@@ -336,31 +336,31 @@ def neighbour(cells, cell):
 
 
 
-#INIZIALIZZAZIONE Celle
+#CELL INITIALIZATION
 cell_array = []
 #Useful variable in the for loop
 x = 0
 y = 0
-init = False #Diventa True quando ha inizializzato tutte le celle
+init = False #Become true when Initialization is completed
 
 
-#Inizializzazione
+#Initialization
 while not init:
 
-    is_alive = random.choices([0,1], weights = (95, 5), k=1)[0]#Sceglie random utilizzando la probabilita'
+    is_alive = random.choices([0,1], weights = (95, 5), k=1)[0]#Randomly spawn cells with probability (Dead 95%, Alive 5 %)
     cell = Cell(x, y, is_alive)#Single object
     x += cell.size
     cell_array.append(cell)
-    if x == screen_width: #Fine riga
+    if x == screen_width: #End of a row
         x = 0
         y += cell.size
-    if y == screen_height:
-        init = True #Celle inizializzate
+    if y == screen_height:#Last row
+        init = True
 
 
-#DISEGNO CELLE
+#DRAWING CELLS
 for cl in cell_array:
-    pygame.draw.rect(screen, cl.color, pygame.Rect(cl.x, cl.y, cl.size, cl.size))#Disegna ogni singola cella
+    pygame.draw.rect(screen, cl.color, pygame.Rect(cl.x, cl.y, cl.size, cl.size))#Draw any single cell
 
 pygame.display.flip() #To update the screen
 
@@ -375,7 +375,7 @@ while not done:
     #FPS
     time.tick(FPS)
 
-    #GESTORE EVENTI
+    #EVENT HANDLER
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #Exit button
             print("Quitting.")
@@ -383,10 +383,9 @@ while not done:
 
 
 
-    #SIMULAZIONE --------------------------------------------------------------------
+    #SIMULATION --------------------------------------------------------------------
 
-    #Esegue l'algoritmo del 'gioco' e aggiorna quindi la schermata (algoritmo di Moore)
-    #Per ogni cella che ha l'attributo alive
+    #Run the algorithm of the game and update the screen (Moore algorithm)
     for cell in cell_array:
         if neighbour(cell_array, cell) in (2, 3): #2 or 3 live neighbours (survive)
             cell.alive = 1
@@ -400,9 +399,9 @@ while not done:
     #Debug
     print("Algorithm succesful.")
 
-    #DISEGNO CELLE
+    #DRAWING CELLS
     for cl in cell_array:
-        pygame.draw.rect(screen, cl.color, pygame.Rect(cl.x, cl.y, cl.size, cl.size))#Disegna ogni singola cella
+        pygame.draw.rect(screen, cl.color, pygame.Rect(cl.x, cl.y, cl.size, cl.size))
     #Debug
     print("Cell loaded to the screen")
 
